fdc7952dcfa9ec72d639be181dc0014d2722f702
Anthony Huang <anthonyhuang07@icloud.com>
2026-09-16 00:25:29 -0400

    testing some edge cases with /sounds/ links

---
+++ functions/sound/[[path]].js
@@ -0,0 +1,9 @@
+export function onRequest() {
+  return new Response("Sound not found.", {
+    status: 404,
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Cache-Control": "no-store",
+    },
+  });
+}
